28458cbccf543471765cbac711a7d70b60eebe86
Ricardo Pérez <ricpelo@gmail.com>
2017-09-28 16:37:13 +0200

    Muestra la suma de los dos números

--- sumar.rb
+++ sumar.rb
@@ -1,4 +1,5 @@
 print "Introduce el primer número: "
-n1 = gets.chomp
+n1 = gets.chomp.to_i
 print "Introduce el segundo número: "
-n2 = gets.chomp
+n2 = gets.chomp.to_i
+puts "La suma vale #{n1 + n2}"
